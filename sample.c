66e24c1a454e26b91263a03e1e6954d202f1bdfd
CodfyForrest <daniil.belskyi1221@gmail.com>
2024-08-04 00:52:46 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-                                
-#define BUFSIZE 256
-                                
+#include <sys/stat.h>
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
-      fprintf(stderr, "Please provide the address of a file as an input.\n");
-      return -1;
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    struct stat file_stat;
+
+    // Check if the file exists and retrieve file statistics
+    if (stat(argv[1], &file_stat) != 0) {
+        perror("Error accessing the file");
+        return -1;
+    }
+
+    // Print the size of the file in bytes
+    printf("Size of the file: %ld bytes\n", file_stat.st_size);
+
+    return 0;
 }
