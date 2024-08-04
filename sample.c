1c77ee49f76ff21792abc16d1602f652b191ecc4
CodfyForrest <daniil.belskyi1221@gmail.com>
2024-08-04 00:44:02 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -4,15 +4,14 @@
                                 
 #define BUFSIZE 256
                                 
+// This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-  char *userName = argv[2];
-  
-  {
-    // GOOD: the user string is encoded by a library routine.
-    char userNameQuoted[1000] = {0};
-    encodeShellString(userNameQuoted, 1000, userName); 
-    char command2[1000] = {0};
-    sprintf(command2, "userinfo -v %s", userNameQuoted);
-    system(command2);
-  }
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
+      fprintf(stderr, "Please provide the address of a file as an input.\n");
+      return -1;
+    }
+    char cmd[BUFSIZE] = "wc -c < ";
+    strcat(cmd, argv[1]);
+    system(cmd);
 }
