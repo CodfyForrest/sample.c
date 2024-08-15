02972d37c89c33cb824e1ef4152ac1b167b27655
CodfyForrest <daniil.belskyi1221@gmail.com>
2024-08-15 17:19:44 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <number>\n", argv[0]);
-        return 1;
-    }
-
-    char *endptr;
-    long index = strtol(argv[1], &endptr, 10);
-
-    if (*endptr != '\0' || index < 1 || index > 4) {
-        fprintf(stderr, "Invalid selection.\n");
-        return 1;
-    }
-
-    char *items[] = {"boat", "car", "truck", "train"};
-    printf("You selected %s\n", items[index-1]);
-
-    return 0;
-}
+								#include <stdlib.h>
+									
+								int main(int argc, char **argv) {
+									char *items[] = {"boat", "car", "truck", "train"};
+									int index = atoi(argv[1]);
+									printf("You selected %s\n", items[index-1]);
+								}	
+							
