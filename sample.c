cd3d16d883ef3f373f7366a236c1d97765f46eb5
CodfyForrest <daniil.belskyi1221@gmail.com>
2024-08-15 16:48:52 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,25 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
-    }
-
-    struct stat file_stat;
-
-    // Check if the file exists and retrieve file statistics
-    if (stat(argv[1], &file_stat) != 0) {
-        perror("Error accessing the file");
-        return -1;
-    }
-
-    // Print the size of the file in bytes
-    printf("Size of the file: %ld bytes\n", file_stat.st_size);
-
-    return 0;
-}
+								#include <stdlib.h>
+									
+								int main(int argc, char **argv) {
+									char *items[] = {"boat", "car", "truck", "train"};
+									int index = atoi(argv[1]);
+									printf("You selected %s\n", items[index-1]);
+								}	
